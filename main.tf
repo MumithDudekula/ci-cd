b4fd705bf09f7b10dc6da2b2bea9205113e9aedf
MumithDudekula <162279828+MumithDudekula@users.noreply.github.com>
2024-03-23 14:00:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "dev" {
     ami = "ami-02d7fd1c2af6eead0"
     instance_type = "t2.micro"
-    key_pair = "nit"
+    key_name = "nit"
     tags = {
       Name = "dev-ec2"
     }
